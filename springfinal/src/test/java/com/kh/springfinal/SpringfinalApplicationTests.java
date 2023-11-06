309524739ffe6dac37e0235f889bb6de2e97d1f1
nnn0008 <secondepect@gmail.com>
2023-11-06 12:16:00 +0900

    최초 업로드

---
+++ springfinal/src/test/java/com/kh/springfinal/SpringfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.springfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
