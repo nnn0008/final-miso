7c638d725ccd932975e4788716629c61a97056aa
lodorive <s_s0309@naver.com>
2023-11-08 15:51:45 +0900

    오류 수정

--- springfinal/src/main/java/com/kh/springfinal/configuration/WebSocketServerConfiguration.java
+++ springfinal/src/main/java/com/kh/springfinal/configuration/WebSocketServerConfiguration.java
@@ -21,11 +21,5 @@ public class WebSocketServerConfiguration implements WebSocketConfigurer{
 		registry.addHandler(websocketServer, "/ws/chat")
 		.addInterceptors(new HttpSessionHandshakeInterceptor())
 		.withSockJS();
-
-	@Override
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		// TODO Auto-generated method stub
-
 	}
-
 }
