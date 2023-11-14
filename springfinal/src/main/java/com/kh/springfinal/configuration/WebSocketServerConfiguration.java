c419fd29ecd056f382289fda959ca13990825b77
lodorive <s_s0309@naver.com>
2023-11-14 19:28:23 +0900

    채팅 파일업로드 구현중

--- springfinal/src/main/java/com/kh/springfinal/configuration/WebSocketServerConfiguration.java
+++ springfinal/src/main/java/com/kh/springfinal/configuration/WebSocketServerConfiguration.java
@@ -1,13 +1,14 @@
 package com.kh.springfinal.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
-import com.kh.springfinal.websocket.FileUploadHandler;
 import com.kh.springfinal.websocket.WebSocketServer;
 
 
@@ -18,18 +19,21 @@ public class WebSocketServerConfiguration implements WebSocketConfigurer{
 	@Autowired
 	private WebSocketServer websocketServer;
 	
-	@Autowired
-	private FileUploadHandler fileUploadHandler;
-
-	
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		registry.addHandler(websocketServer, "/ws/chat")
 		.addInterceptors(new HttpSessionHandshakeInterceptor())
 		.withSockJS(); //spring 표준
-		
-		registry.addHandler(fileUploadHandler, "/ws/file-upload")
-        .addInterceptors(new HttpSessionHandshakeInterceptor())
-        .withSockJS();
+
 	}
+	
+	//웹소켓 허용량을 늘리는 도구 추가
+	@Bean
+	public ServletServerContainerFactoryBean containerFactoryBean() {
+		ServletServerContainerFactoryBean bean = new ServletServerContainerFactoryBean();
+		bean.setMaxTextMessageBufferSize(Integer.MAX_VALUE);
+		return bean;
+	}
+	
+	
 }
