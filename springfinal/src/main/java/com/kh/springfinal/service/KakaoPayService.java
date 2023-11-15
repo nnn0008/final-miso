e588a952f1670b9dfba8666b4f89d3decbe71220
jsw2840 <jis2843@naver.com>
2023-11-15 21:05:29 +0900

    정기결제 구현중

--- springfinal/src/main/java/com/kh/springfinal/service/KakaoPayService.java
+++ springfinal/src/main/java/com/kh/springfinal/service/KakaoPayService.java
@@ -20,17 +20,8 @@ import com.kh.springfinal.vo.PurchaseListVO;
 
 public interface KakaoPayService {
 	KakaoPayReadyResponseVO ready(KakaoPayReadyRequestVO request) throws URISyntaxException;
-	KakaoPayRegularReadyResponseVO ready2(KakaoPayReadyRequestVO request) throws URISyntaxException;
-	
 	KakaoPayApproveResponseVO approve(KakaoPayApproveRequestVO request)throws URISyntaxException;
-	KakaoPayApproveResponseVO approve2(KakaoPayRegularApproveRequestVO request)throws URISyntaxException;
-	
 	KakaoPayDetailResponseVO detail(KakaoPayDetailRequestVO request)throws URISyntaxException;
-	KakaoPayRegularDetailResponseVO detail2(KakaoPayRegularDetailRequestVO request)throws URISyntaxException;
-	
 	KakaoPayCancelResponseVO cancel(KakaoPayCancelRequestVO request)throws URISyntaxException;
-	KakaoPayRegularCancelResponseVO cancel2(KakaoPayRegularCancelRequestVO request)throws URISyntaxException;
-	
-	
 	KakaoPayReadyRequestVO convert(PurchaseListVO listVO);
 }
--- springfinal/src/main/java/com/kh/springfinal/vo/KakaoPayRegularReadyRequestVO.java
+++ springfinal/src/main/java/com/kh/springfinal/vo/KakaoPayRegularReadyRequestVO.java
@@ -8,13 +8,13 @@ import lombok.NoArgsConstructor;
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class KakaoPayRegularReadyRequestVO {
 	private String cid; //가맹점 ID
-	private String partner_order_id; // 가맹점 주분번호
-	private String partner_user_id; // 가맹점 회원 아이디
-	private String item_name;//상품명
+	private String partnerOrderId; // 가맹점 주분번호
+	private String partnerUserId; // 가맹점 회원 아이디
+	private String itemName;//상품명
 	private int quantity; //상품수량
-	private int total_amount; //구매 금액
-	private int tax_free_amount; //비과세 금액(0원으로 설정)
- 	private String approval_url; // 성공시 URL
-	private String cancel_url; // 취소시 URL
-	private String fail_url; // 실패시URL
+	private int totalAmount; //구매 금액
+	private int taxFreeAmount; //비과세 금액(0원으로 설정)
+ 	private String approvalUrl; // 성공시 URL
+	private String cancelUrl; // 취소시 URL
+	private String failUrl; // 실패시URL
 }
