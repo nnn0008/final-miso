2a96da1667256207748eafa504c538eac87a428d
Havenlife <taeseok511@gmail.com>
2023-11-22 14:10:46 +0900

    react 필요 구문

--- springfinal/src/main/java/com/kh/springfinal/reactRest/OneReactController.java
+++ springfinal/src/main/java/com/kh/springfinal/reactRest/OneReactController.java
@@ -5,24 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kh.springfinal.dao.MemberDao;
-import com.kh.springfinal.dto.MemberDto;
+import com.kh.springfinal.dao.OneDao;
+import com.kh.springfinal.dto.OneDto;
 
-//@CrossOrigin
 @CrossOrigin(value = {"http://localhost:3000", "http://localhost:5500"})
 @RestController
-@RequestMapping("/memberList")
-public class MemberReactController {
-	
+@RequestMapping("/react/onereq")
+public class OneReactController {
 	@Autowired
-	private MemberDao memberDao;
+	private OneDao oneDao;
 	
 	@GetMapping("/")
-	public List<MemberDto> selectListOld() {
-		return memberDao.selectListOld();
+	public List<OneDto> selectList(){
+		return oneDao.selectList();
 	}
 }
