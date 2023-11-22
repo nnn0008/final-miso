38e054cf86321dee2c1fcca3f84e49a612db2d13
Havenlife <taeseok511@gmail.com>
2023-11-22 13:19:17 +0900

    react oneList보내는 구문 구현 중

---
+++ springfinal/src/main/java/com/kh/springfinal/reactRest/OneReactController.java
@@ -0,0 +1,27 @@
+package com.kh.springfinal.reactRest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.springfinal.dao.OneDao;
+import com.kh.springfinal.dto.OneDto;
+
+
+@CrossOrigin(value = {"http://localhost:3000", "http://localhost:5500"})
+@RestController
+@RequestMapping("/rest/onereq")
+public class OneReactController {
+	@Autowired
+	private OneDao oneDao;
+	
+	@GetMapping("/")
+	public List<OneDto> selectList(){
+		return oneDao.selectList();
+	}
+}
