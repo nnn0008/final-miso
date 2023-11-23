3eedb80909b62d5927834138bed5547d0935a4e1
nnn0008 <secondepect@gmail.com>
2023-11-23 21:29:20 +0900

    현재까지 한거 업로드(사진게시판 수정중)

--- springfinal/src/main/java/com/kh/springfinal/rest/ReplyRestController.java
+++ springfinal/src/main/java/com/kh/springfinal/rest/ReplyRestController.java
@@ -20,12 +20,15 @@ import com.kh.springfinal.dao.ClubBoardDao;
 import com.kh.springfinal.dao.ClubBoardReplyDao;
 import com.kh.springfinal.dao.ClubMemberDao;
 import com.kh.springfinal.dao.MemberDao;
+import com.kh.springfinal.dao.PhotoReplyDao;
 import com.kh.springfinal.dto.ClubBoardDto;
 import com.kh.springfinal.dto.ClubBoardReplyDto;
 import com.kh.springfinal.dto.ClubMemberDto;
 import com.kh.springfinal.dto.MemberDto;
+import com.kh.springfinal.dto.PhotoReplyDto;
 import com.kh.springfinal.vo.ClubBoardReplyMemberVO;
 import com.kh.springfinal.vo.ClubBoardReplyVO;
+import com.kh.springfinal.vo.PhotoReplyVO;
 
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
@@ -44,6 +47,9 @@ public class ReplyRestController {
 	
 	@Autowired
 	private ClubMemberDao clubMemberDao;
+	
+	@Autowired
+	private PhotoReplyDao photoReplyDao;
 	
 	@PostMapping("/insert")
 	public ResponseEntity<Map<String, Object>> insert(HttpSession session,
@@ -169,5 +175,70 @@ public class ReplyRestController {
 	@PostMapping("/edit")
 	public void edit(@ModelAttribute ClubBoardReplyDto clubBoardreplyDto) {
 		clubBoardReplyDao.edit(clubBoardreplyDto.getClubBoardReplyNo(), clubBoardreplyDto.getClubBoardReplyContent());
+	}
+	
+	/////////////////////////
+	//사진 게시판 관련
+	@PostMapping("/photo/insert")
+	public void insert(@RequestParam String photoReplyContent, @RequestParam int clubNo, HttpSession session,
+			@RequestParam int photoNo, @RequestParam(required=false) Integer photoReplyParent) {
+		PhotoReplyDto photoReplyDto = new PhotoReplyDto();
+		int photoReplyNo = photoReplyDao.sequence();
+		//로그인한 유저에 대한 정보
+		String loginId = (String) session.getAttribute("name");
+		ClubMemberDto clubMemberDto = clubBoardDao.selectOneClubMemberNo(loginId, clubNo);
+		
+		photoReplyDto.setClubMemberNo(clubMemberDto.getClubMemberNo()); //로그인 한 유저의 클럽멤버번호
+		photoReplyDto.setPhotoReplyNo(photoReplyNo);
+		photoReplyDto.setPhotoNo(photoNo);
+		photoReplyDto.setPhotoReplyContent(photoReplyContent);
+		
+		if(photoReplyParent == null) { //원댓글이라면			
+			photoReplyDto.setPhotoReplyGroup(photoReplyNo); //원댓글은 자기 번호가 그룹
+			photoReplyDto.setPhotoReplyParent(photoReplyParent); //원댓글은 부모번호가 없다
+		}
+		else {//본댓글이라면
+			photoReplyDto.setPhotoReplyParent(photoReplyParent); //대댓글은 부모번호가 원댓글이다
+			photoReplyDto.setPhotoReplyGroup(photoReplyParent); //대댓글은 부모번호의 그룹에 속한다
+		}
+		
+		photoReplyDao.insert(photoReplyDto);
+	}
+	
+	@PostMapping("/photo/list")
+	public List<PhotoReplyVO> list(@RequestParam int photoNo, HttpSession session, @RequestParam int clubNo){
+		List<PhotoReplyDto> dtoList = photoReplyDao.selectList(photoNo);
+		List<PhotoReplyVO> voList = new ArrayList<>();
+		
+		String loginId = (String)session.getAttribute("name");
+		ClubMemberDto clubMemberDto = clubBoardDao.selectOneClubMemberNo(loginId, clubNo);
+		
+		for(PhotoReplyDto dto : dtoList) {
+			PhotoReplyVO vo = new PhotoReplyVO();
+			vo.setClubMemberNo(dto.getClubMemberNo());
+			vo.setPhotoNo(dto.getPhotoNo());
+			vo.setPhotoReplyContent(dto.getPhotoReplyContent());
+			vo.setPhotoReplyDate(dto.getPhotoReplyDate());
+			vo.setPhotoReplyGroup(dto.getPhotoReplyGroup());
+			vo.setPhotoReplyNo(dto.getPhotoReplyNo());
+			vo.setPhotoReplyParent(dto.getPhotoReplyParent());
+			
+			//로그인한 유저와 댓글 작성자를 비교할 변수 하나를 추가
+										//댓글 작성자의 클럽번호 vs 로그인한 유저의 클럽번호
+			boolean isMatch = dto.getClubMemberNo() == clubMemberDto.getClubMemberNo();
+			//dto에서 얻은 유저의 멤버 아이디로 vo의 writer와 id를 추가
+			ClubMemberDto writerDto = clubMemberDao.selectOne(dto.getClubMemberNo());
+			MemberDto memberDto = memberDao.loginId(writerDto.getClubMemberId());
+			vo.setPhotoReplyId(writerDto.getClubMemberId());
+			vo.setPhotoReplyWriter(memberDto.getMemberName());
+			vo.setMatch(isMatch);
+			voList.add(vo);
+		}
+		return voList;
+	}
+	
+	@PostMapping("/photo/delete")
+	public void photoDelete(@RequestParam int photoReplyNo) {
+		photoReplyDao.delete(photoReplyNo);
 	}
 }
