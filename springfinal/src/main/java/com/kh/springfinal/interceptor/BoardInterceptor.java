70d14cefb0b1e8528c6066dd513dffbd76d4f681
nnn0008 <secondepect@gmail.com>
2023-12-05 19:44:38 +0900

    마지막 업로드

--- springfinal/src/main/java/com/kh/springfinal/interceptor/BoardInterceptor.java
+++ springfinal/src/main/java/com/kh/springfinal/interceptor/BoardInterceptor.java
@@ -1,87 +1,87 @@
-//package com.kh.springfinal.interceptor;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//
-//import com.kh.springfinal.dao.ClubBoardDao;
-//import com.kh.springfinal.dao.ClubDao;
-//import com.kh.springfinal.dao.ClubMemberDao;
-//import com.kh.springfinal.dto.ClubBoardDto;
-//import com.kh.springfinal.dto.ClubMemberDto;
-//
-//import lombok.extern.slf4j.Slf4j;
-//@Slf4j
-//@Component
-//public class BoardInterceptor implements HandlerInterceptor{
-//	
-//	@Autowired
-//	private ClubDao clubDao;
-//	@Autowired
-//	private ClubBoardDao clubBoardDao;
-//	@Autowired
-//	private ClubMemberDao clubMemberDao;
-//	
-//	//게시판에서 일어날 수 있는 인터셉터
-//	// - 게시판 / 글쓰기 / 사진첩은 clubNo 사용
-//	// - 게시글 상세에서는 clubBoardNo 사용
-//	// - 가입 여부를 따져서 글쓰기 게시판을 들어갈 수 있는지를 따져본다.
-//	// - 동호회 홈( /club/detail?clubNo=?? ) 으로 리 다이렉트 시켜준다
-//	
-//	@Override
-//	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-//			throws Exception {
-//		HttpSession session = request.getSession();
-//		String memberId = (String)session.getAttribute("name");
-//		boolean isLogin = memberId != null;
-//		
-//		//글 번호 혹은 게시글 번호
-//		String stringClubNo = request.getParameter("clubNo");
-//		String stringClubBoardNo = request.getParameter("clubBoardNo");
-//		log.debug("isLogin = {}", isLogin);
-//		log.debug("stringClubNo = {}", stringClubNo);
-//		log.debug("stringClubBoardNo = {}", stringClubBoardNo);
-//		//클럽 번호가 있는 곳은?
-//		if(!isLogin) { //가장 처음으로 로그인 안된 계정은 로그인으로 리 다이렉트
-//			response.sendRedirect(request.getContextPath() + "/member/login");
-//			return false;
-//		}
-//		
-//		else if(stringClubBoardNo != null) { //게시판 글쓰기 화면은 클럽 홈으로 리다이렉트
-//			int clubBoardNo = Integer.parseInt(stringClubBoardNo);
-//			ClubBoardDto clubBoardDto = clubBoardDao.selectOnes(clubBoardNo);
-//			ClubMemberDto clubMemberDto = clubBoardDao.selectOneClubMemberNo(memberId, clubBoardDto.getClubNo());
-//			if(clubMemberDto == null) { //로그인은 됐으나 가입은 안 된경우
-//				response.sendRedirect(request.getContextPath() + "/club/detail?clubNo=" + clubBoardDto.getClubNo());
-//				return false;
-//			}
-//			else { //로그인도 됐고 가입도 된 경우라면?
-//				return true;
-//			}
-//		}
-//		
-//		else if(stringClubNo != null) { //게시판 상세도 마찬가지
-//			int clubNo = Integer.parseInt(stringClubNo);
-//			ClubMemberDto clubMemberDto = clubBoardDao.selectOneClubMemberNo(memberId, clubNo);
-//			if(clubMemberDto == null) { //로그인은 됐으나 가입은 안 된경우
-//				response.sendRedirect(request.getContextPath() + "/club/detail?clubNo=" + clubNo);
-//				return false;
-//			}
-//			else { //로그인도 됐고 가입도 된 경우라면?
-//				return true;
-//			}
-//		}
-//		
-//		else {
-//			return true; 
-//		}
-//		
-//	}
-//	
-//	
-//	
-//}
+package com.kh.springfinal.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.kh.springfinal.dao.ClubBoardDao;
+import com.kh.springfinal.dao.ClubDao;
+import com.kh.springfinal.dao.ClubMemberDao;
+import com.kh.springfinal.dto.ClubBoardDto;
+import com.kh.springfinal.dto.ClubMemberDto;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@Component
+public class BoardInterceptor implements HandlerInterceptor{
+	
+	@Autowired
+	private ClubDao clubDao;
+	@Autowired
+	private ClubBoardDao clubBoardDao;
+	@Autowired
+	private ClubMemberDao clubMemberDao;
+	
+	//게시판에서 일어날 수 있는 인터셉터
+	// - 게시판 / 글쓰기 / 사진첩은 clubNo 사용
+	// - 게시글 상세에서는 clubBoardNo 사용
+	// - 가입 여부를 따져서 글쓰기 게시판을 들어갈 수 있는지를 따져본다.
+	// - 동호회 홈( /club/detail?clubNo=?? ) 으로 리 다이렉트 시켜준다
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		String memberId = (String)session.getAttribute("name");
+		boolean isLogin = memberId != null;
+		
+		//글 번호 혹은 게시글 번호
+		String stringClubNo = request.getParameter("clubNo");
+		String stringClubBoardNo = request.getParameter("clubBoardNo");
+		log.debug("isLogin = {}", isLogin);
+		log.debug("stringClubNo = {}", stringClubNo);
+		log.debug("stringClubBoardNo = {}", stringClubBoardNo);
+		//클럽 번호가 있는 곳은?
+		if(!isLogin) { //가장 처음으로 로그인 안된 계정은 로그인으로 리 다이렉트
+			response.sendRedirect(request.getContextPath() + "/member/login");
+			return false;
+		}
+		
+		else if(stringClubBoardNo != null) { //게시판 글쓰기 화면은 클럽 홈으로 리다이렉트
+			int clubBoardNo = Integer.parseInt(stringClubBoardNo);
+			ClubBoardDto clubBoardDto = clubBoardDao.selectOnes(clubBoardNo);
+			ClubMemberDto clubMemberDto = clubBoardDao.selectOneClubMemberNo(memberId, clubBoardDto.getClubNo());
+			if(clubMemberDto == null) { //로그인은 됐으나 가입은 안 된경우
+				response.sendRedirect(request.getContextPath() + "/club/detail?clubNo=" + clubBoardDto.getClubNo());
+				return false;
+			}
+			else { //로그인도 됐고 가입도 된 경우라면?
+				return true;
+			}
+		}
+		
+		else if(stringClubNo != null) { //게시판 상세도 마찬가지
+			int clubNo = Integer.parseInt(stringClubNo);
+			ClubMemberDto clubMemberDto = clubBoardDao.selectOneClubMemberNo(memberId, clubNo);
+			if(clubMemberDto == null) { //로그인은 됐으나 가입은 안 된경우
+				response.sendRedirect(request.getContextPath() + "/club/detail?clubNo=" + clubNo);
+				return false;
+			}
+			else { //로그인도 됐고 가입도 된 경우라면?
+				return true;
+			}
+		}
+		
+		else {
+			return true; 
+		}
+		
+	}
+	
+	
+	
+}
--- springfinal/src/main/java/com/kh/springfinal/interceptor/ChatRoomIntercepter.java
+++ springfinal/src/main/java/com/kh/springfinal/interceptor/ChatRoomIntercepter.java
@@ -1,75 +1,75 @@
-//package com.kh.springfinal.interceptor;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//
-//import com.kh.springfinal.dao.ChatRoomDao;
-//import com.kh.springfinal.error.AuthorityException;
-//
-////회원인지 아닌지 검사하여 비회원을 차단하는 인터셉터 구현
-//
-//@Component
-//public class ChatRoomIntercepter implements HandlerInterceptor {
-//
-//    @Autowired
-//    private ChatRoomDao chatRoomDao;
-//
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-//            throws Exception {
-//
-//        // URL 경로에서 chatRoomNo 추출
-//        String path = request.getRequestURI();
-//        String[] pathSegments = path.split("/");
-//        String chatRoomNoStr = pathSegments[pathSegments.length - 1];
-//
-//        // chatRoomNo를 정수로 변환
-//        int chatRoomNo;
-//        try {
-//            chatRoomNo = Integer.parseInt(chatRoomNoStr);
-//        } catch (NumberFormatException e) {
-//            // chatRoomNo가 정수로 변환할 수 없는 경우 처리
-//        	throw new AuthorityException("error");
-//        }
-//
-//        // 세션에서 회원 아이디 추출
-//        HttpSession session = request.getSession();
-//        String memberId = (String) session.getAttribute("name");
-//
-//     // 채팅방 회원 여부 확인
-//        if (isChatRoomMember(memberId, chatRoomNo) || isMeetingRoomMember(memberId, chatRoomNo) 
-//        		|| isOneChatRoomMember(memberId, chatRoomNo)) {
-//            // 채팅방 회원이면 통과
-//            return true;
-//        } else {
-//            // 채팅방 회원이 아니면 chatRoomError.jsp로 포워딩
-//            throw new AuthorityException("채팅방 멤버가 아닙니다.");
-//        }
-//    }
-//
-//
-//    private boolean isChatRoomMember(String memberId, int chatRoomNo) {
-//        int result = chatRoomDao.isChatRoomMember(memberId, chatRoomNo);
-//
-//        // 0이면 채팅방 회원이 아님
-//        return result > 0;
-//    }
-//    
-//    private boolean isMeetingRoomMember(String memberId, int chatRoomNo) {
-//    	int result = chatRoomDao.isMeetingRoomMember(memberId, chatRoomNo);
-//    	return result > 0;
-//    }
-//    
-//    private boolean isOneChatRoomMember(String memberId, int chatRoomNo) {
-//    	int result = chatRoomDao.isOneChatRoomMember(memberId, chatRoomNo);
-//    	return result > 0;
-//    }
-//    
-//    
-//}
-//
+package com.kh.springfinal.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.kh.springfinal.dao.ChatRoomDao;
+import com.kh.springfinal.error.AuthorityException;
+
+//회원인지 아닌지 검사하여 비회원을 차단하는 인터셉터 구현
+
+@Component
+public class ChatRoomIntercepter implements HandlerInterceptor {
+
+    @Autowired
+    private ChatRoomDao chatRoomDao;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        // URL 경로에서 chatRoomNo 추출
+        String path = request.getRequestURI();
+        String[] pathSegments = path.split("/");
+        String chatRoomNoStr = pathSegments[pathSegments.length - 1];
+
+        // chatRoomNo를 정수로 변환
+        int chatRoomNo;
+        try {
+            chatRoomNo = Integer.parseInt(chatRoomNoStr);
+        } catch (NumberFormatException e) {
+            // chatRoomNo가 정수로 변환할 수 없는 경우 처리
+        	throw new AuthorityException("error");
+        }
+
+        // 세션에서 회원 아이디 추출
+        HttpSession session = request.getSession();
+        String memberId = (String) session.getAttribute("name");
+
+     // 채팅방 회원 여부 확인
+        if (isChatRoomMember(memberId, chatRoomNo) || isMeetingRoomMember(memberId, chatRoomNo) 
+        		|| isOneChatRoomMember(memberId, chatRoomNo)) {
+            // 채팅방 회원이면 통과
+            return true;
+        } else {
+            // 채팅방 회원이 아니면 chatRoomError.jsp로 포워딩
+            throw new AuthorityException("채팅방 멤버가 아닙니다.");
+        }
+    }
+
+
+    private boolean isChatRoomMember(String memberId, int chatRoomNo) {
+        int result = chatRoomDao.isChatRoomMember(memberId, chatRoomNo);
+
+        // 0이면 채팅방 회원이 아님
+        return result > 0;
+    }
+    
+    private boolean isMeetingRoomMember(String memberId, int chatRoomNo) {
+    	int result = chatRoomDao.isMeetingRoomMember(memberId, chatRoomNo);
+    	return result > 0;
+    }
+    
+    private boolean isOneChatRoomMember(String memberId, int chatRoomNo) {
+    	int result = chatRoomDao.isOneChatRoomMember(memberId, chatRoomNo);
+    	return result > 0;
+    }
+    
+    
+}
+
